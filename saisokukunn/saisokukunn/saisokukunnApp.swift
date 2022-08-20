324176c60e0c2815d611b60540f57defe9c93704
H-Yuuki <yuukirinnma@gmail.com>
2022-08-20 20:40:44 +0900

    add: 初期ファイル作成

---
+++ saisokukunn/saisokukunn/saisokukunnApp.swift
@@ -0,0 +1,17 @@
+//
+//  saisokukunnApp.swift
+//  saisokukunn
+//
+//  Created by 濵田　悠樹 on 2022/08/20.
+//
+
+import SwiftUI
+
+@main
+struct saisokukunnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
