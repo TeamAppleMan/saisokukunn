4f20d072a9a7a123130651d806d74de97a5c8f63
Kota <maedakoutaa@gmail.com>
2022-08-22 10:13:45 +0900

    ContentViewの削除

---
+++ saisokukunn/saisokukunn/View/Main/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  saisokukunn
+//
+//  Created by 前田航汰 on 2022/08/22.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
