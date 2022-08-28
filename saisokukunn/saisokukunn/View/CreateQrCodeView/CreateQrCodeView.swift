48d2e5f30123f318eecf5b8b70cc9a1b05e386de
Kota <maedakoutaa@gmail.com>
2022-08-28 17:06:14 +0900

    CreateQrCodeViewの完成

--- saisokukunn/saisokukunn/View/CreateQrCodeView/CreateQrCodeView.swift
+++ saisokukunn/saisokukunn/View/CreateQrCodeView/CreateQrCodeView.swift
@@ -10,23 +10,47 @@ import SwiftUI
 struct CreateQrCodeView: View {
 
     var body: some View {
+        let displayBounds = UIScreen.main.bounds
+        let displayWidth = displayBounds.width
+        let displayHeight = displayBounds.height
+        let textColor = Color.init(red: 0.3, green: 0.3, blue: 0.3)
 
-        VStack {
-            Text("QRの表示")
-                .font(.title)
+        // 画像の高さ
+        let imageHeight = displayHeight/2.0
+        // QRコードボックスのサイズ
+        let squareQrcodeBoxSize = displayWidth - 120.0
+        // QRボックスと画像を重ねるためにずらすoffsetサイズ
+        let packOffsetSize = 3*imageHeight/12
+
+        VStack(spacing: 0) {
+
+            QrCodeBoxView()
+                .frame(width: squareQrcodeBoxSize, height: squareQrcodeBoxSize)
                 .padding()
-            Text("通信が終わったら、")
-                .font(.title)
-            Text("自動で画面帰らせたい")
-                .font(.title)
+
+            Image("SendDateWithMan")
+                .resizable()
+                .scaledToFit()
+                .frame(height: imageHeight, alignment: .center)
+                .offset(x: 0, y: -packOffsetSize)
+
+            Text("相手側のアプリでスキャンして下さい。")
+                .foregroundColor(textColor)
+                .font(.callout)
+                .offset(x: 0, y: -packOffsetSize)
+            Text("スキャン後自動で画面は戻ります。")
+                .foregroundColor(textColor)
+                .font(.callout)
+                .offset(x: 0, y: -packOffsetSize)
+            Spacer()
         }
 
     }
 
 }
 
-//struct CreateQrCodeView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        CreateQrCodeView()
-//    }
-//}
+struct CreateQrCodeView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateQrCodeView()
+    }
+}
