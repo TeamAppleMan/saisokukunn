20d50715529c7132a6a502d81c8c9a10d809cc67
Kota <maedakoutaa@gmail.com>
2022-09-01 00:32:22 +0900

    signupViewの完成

--- saisokukunn/saisokukunn/View/SignUpView/SignUpView.swift
+++ saisokukunn/saisokukunn/View/SignUpView/SignUpView.swift
@@ -15,26 +15,76 @@ struct SignUpView: View {
     let registerUser = RegisterUser()
 
     var body: some View{
+        let textColor = Color.init(red: 0.3, green: 0.3, blue: 0.3)
+        let inputAccessoryHorizontalMargin = 25.0
+
+        let displayBounds = UIScreen.main.bounds
+        let displayHeight = displayBounds.height
+        let imageHeight = displayHeight/3.25
 
         NavigationView{
+
             VStack{
+                // 上半分を画像
+                VStack {
+                    HStack() {
+                        Image("InputUsernameWithMan")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: imageHeight, alignment: .center)
+                    }
+                }.padding(.bottom, 50)
+
+                // TODO: ナニコレ
                 NavigationLink(destination: MainView(), isActive: $isActiveMainView){
                     EmptyView()
                 }
-                TextField("名前",text: $userName)
-                    .textFieldStyle(RoundedBorderTextFieldStyle()).padding()
-                Button("サインイン"){
-                    Task{
-                        do{
-                            try await registerUser.signIn(userName:userName)
-                            // MainViewへ画面遷移
-                            isActiveMainView = true
+
+                // 下半分をインプット部分
+                VStack {
+
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("表示名")
+                            .foregroundColor(textColor)
+                        TextField("表示名を入力して下さい。",text: $userName)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }.padding(inputAccessoryHorizontalMargin)
+
+                    Button(action: {
+                        Task{
+                            do{
+                                try await registerUser.signIn(userName:userName)
+                                // MainViewへ画面遷移
+                                isActiveMainView = true
+                            }
+                            catch{
+                                print("サインインに失敗しました")
+                            }
                         }
-                        catch{
-                            print("サインインに失敗しました")
-                        }
+                    }) {
+                        Text("登録")
+                            .frame(width: 150.0, alignment: .center)
+                            .padding()
+                            .accentColor(Color.white)
+                            .background(Color.gray)
+                            .cornerRadius(25)
+                            .shadow(color: Color.gray, radius: 10, x: 0, y: 3)
+                            .padding()
                     }
+
+                    Spacer()
+
+                    VStack {
+                        Text("貸し借りの相手に表示される名前です。")
+                            .foregroundColor(textColor)
+                            .font(.callout)
+                        Text("適切な名前を登録しましょう。")
+                            .foregroundColor(textColor)
+                            .font(.callout)
+                    }.padding()
+
                 }
+
             }.onAppear {
                 // uidが存在するならMainViewへ移動
                 if let uid = Auth.auth().currentUser?.uid {
@@ -42,6 +92,8 @@ struct SignUpView: View {
                     isActiveMainView = true
                 }
             }// onAppearここまで
+
+            Spacer()
         }
         .navigationBarHidden(true)
     }
