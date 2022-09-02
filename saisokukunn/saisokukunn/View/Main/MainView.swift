928c5a2f7479fea001908d46d54ec8f516a6a0ab
mikekondo <foad5969@gmail.com>
2022-09-03 07:57:13 +0900

    add: 締め切り日がマイナス時に0にする

--- saisokukunn/saisokukunn/View/Main/MainView.swift
+++ saisokukunn/saisokukunn/View/Main/MainView.swift
@@ -280,6 +280,8 @@ struct MainView: View {
         var limitDay = Int(limit/60/60/24)
         if(limit>0){
             limitDay += 1
+        }else{
+            limitDay = 0
         }
         return Int(limitDay)
     }
