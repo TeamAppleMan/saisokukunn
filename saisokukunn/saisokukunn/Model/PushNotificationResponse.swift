f211d03d272fe8df62f2faf6fb8576b7e320af97
H-Yuuki <yuukirinnma@gmail.com>
2022-09-18 13:01:21 +0900

    add: プッシュ通知を可能にする

---
+++ saisokukunn/saisokukunn/Model/PushNotificationResponse.swift
@@ -0,0 +1,13 @@
+//
+//  PushNotificationResponse.swift
+//  saisokukunn
+//
+//  Created by 濵田　悠樹 on 2022/09/15.
+//
+
+import Foundation
+
+class PushNotificationResponse: Codable {
+    public var id: Int
+    public var result: String
+}
