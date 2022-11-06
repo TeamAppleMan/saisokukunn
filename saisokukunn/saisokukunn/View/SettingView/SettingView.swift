277de0881153eafdc86bd3684e2d92ee67a88962
Kota <maedakoutaa@gmail.com>
2022-11-06 14:17:25 +0900

    add: SettingViewの追加

---
+++ saisokukunn/saisokukunn/View/SettingView/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  saisokukunn
+//
+//  Created by 前田航汰 on 2022/11/06.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
